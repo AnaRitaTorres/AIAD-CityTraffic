9c1f5942697ffe3a32ce0e2855e343c8149dd8f7
Ana Rita Torres <anaritatorres96@outlook.pt>
2017-12-09 20:52:26 +0000

    data print done

--- src/gui/Statistics.java
+++ src/gui/Statistics.java
@@ -3,7 +3,7 @@ package gui;
 public class Statistics {
 	private int totalAccidents = 0;
 	private float avgAccidents = 0;
-	private float totalDist = 0;	//TODO
+	private float totalDistance = 0;	//TODO
 	private float avgDistance = 0;	//TODO
 	private float avgTravelTime = 0;	//TODO later or not at all se nao houver tempo
 	private float totalTimeWaitingTrafficLight = 0;	//TODO
@@ -15,6 +15,46 @@ public class Statistics {
 	/*public Statistics(disp){
 		this.disp=disp;
 	}*/
+	
+	public int getTotalAccidents() {
+		return totalAccidents;
+	}
+	
+	public float getAvgAccidents() {
+		return avgAccidents;
+	}
+	
+	public float getTotalDistance() {
+		return totalDistance;
+	}
+	
+	public float getAvgDistance() {
+		return avgDistance;
+	}
+	
+	public float getAvgTravelTime() {
+		return avgTravelTime;
+	}
+	
+	public float getTotalTimeWaitingTrafficLight() {
+		return totalTimeWaitingTrafficLight;
+	}
+	
+	public float getAvgTimeWaitingTrafficLight() {
+		return avgTimeWaitingTrafficLight;
+	}
+	
+	public float getTotalTimeWaitingTraffic(){
+		return  totalTimeWaitingTraffic;
+	}
+	
+	public float getAvgTimeWaitingTraffic() {
+		return  avgTimeWaitingTraffic;
+	}
+	
+	public float getAvgTimeNotMoving() {
+		return avgTimeNotMoving;
+	}
 	
 	public void updateTotalAccidents(){
 		totalAccidents ++;
