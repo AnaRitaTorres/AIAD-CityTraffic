23e512fa7b88367deb21371e77671922f5d36f1e
margaridaviterbo <margaridaviterbo@hotmail.com>
2017-12-09 20:49:19 +0000

    stats for accidents and distances done, other stats missing

--- src/gui/Statistics.java
+++ src/gui/Statistics.java
@@ -3,10 +3,10 @@ package gui;
 public class Statistics {
 	private int totalAccidents = 0;
 	private float avgAccidents = 0;
-	private float totalDist = 0;	//TODO
-	private float avgDistance = 0;	//TODO
+	private double totalDistance = 0;
+	private double avgDistance = 0;
 	private float avgTravelTime = 0;	//TODO later or not at all se nao houver tempo
-	private float totalTimeWaitingTrafficLight = 0;	//TODO
+	private float totalTimeWaitingTrafficLight = 0;	//TODO vou aqui
 	private float avgTimeWaitingTrafficLight = 0;	//TODO
 	private float totalTimeWaitingTraffic = 0;	//TODO
 	private float avgTimeWaitingTraffic = 0;	//TODO
@@ -22,9 +22,40 @@ public class Statistics {
 	}
 	
 	public void updateAvgAccidents(int numCars){
-		System.out.println(numCars);
 		avgAccidents = (float)totalAccidents/(float)numCars;
 		updateDisplay();
+	}
+	
+	public void updateTotalDistance(double dist){
+		totalDistance += dist;
+	}
+	
+	public void updateAvgDistance(int numCars){
+		avgDistance = totalDistance/(float)numCars;
+	}
+	
+	//update avgTravelTime
+	
+	public void updateTotalTimeWaitingTrafficLight(float time){
+		totalTimeWaitingTrafficLight += time;
+		System.out.println("total " + totalTimeWaitingTrafficLight);
+	}
+	
+	public void updateAvgTimeWaitingTrafficLight(int numCars){
+		avgTimeWaitingTrafficLight = totalTimeWaitingTrafficLight/(float)numCars;
+		System.out.println("avg " + avgTimeWaitingTrafficLight);
+	}
+	
+	public void updateTotalTimeWaitingTraffic(float time){
+		totalTimeWaitingTraffic += time;
+	}
+	
+	public void updateAvgTimeWaitingTraffic(int numCars){
+		avgTimeWaitingTraffic = totalTimeWaitingTraffic/(float)numCars;
+	}
+	
+	public void updateAvgTimeNotMoving(int numCars){
+		avgTimeNotMoving = (totalTimeWaitingTrafficLight + totalTimeWaitingTraffic)/(float)numCars;
 	}
 	
 	public void updateDisplay(){
